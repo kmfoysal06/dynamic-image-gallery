738e7c857b7ab7cb39e768d34e063c88935be43e
kazi mohammed foysal <foysalkazimd01@gmail.com>
2024-01-08 20:02:02 +0600

    completed

--- functions.php
+++ functions.php
@@ -18,3 +18,26 @@ function getImages($dir,array $explode = array('.','..')){
 	closedir($open_dir);
 	}
 
+function upload_images($image,$dir='images',array $file_types = ['image/jpeg','image/png','image/gif','image/jpg','image/svg','image/webp']){
+	if(in_array($image['type'], $file_types)){
+		if($image['size'] < 2*1024*1024 || $image['size'] > 1){
+			if(!($image['error'] >= 1)){
+				$fileNameArr = explode('.', $image['name']);
+				$fileName = $fileNameArr[0];
+				$fileExt = $fileNameArr[count($fileNameArr) - 1];
+				$uniqueName = uniqid($fileName) . $fileExt ;
+				if(move_uploaded_file($image['tmp_name'], $dir.'/'.$uniqueName)){
+					return 'uploaded';
+				}else{
+					return 'some_error';
+				}
+			}else{
+				return 'some_error';
+			}
+		}else{
+			return 'wrong_size';
+		}
+	}else{
+		return 'wrong_ext';
+	}
+}
--- index.php
+++ index.php
@@ -6,6 +6,29 @@ if($get_images === false){
 	die("There is No Directory");
 }
 
+if(isset($_POST['submit'])){
+	if(isset($_FILES['image'])){
+		if(!empty($_FILES['image'])){
+			switch (upload_images($_FILES['image'],'images')) {
+						case 'uploaded':
+							echo 'image uploaded successuly!';
+							break;
+						case 'some_error':
+							echo 'Image can not be uploaded due to some error';
+							break;
+						case 'wrong_size':
+							echo 'Please Upload Image Between Range Of 1kb to 2mb';
+							break;
+						case 'wrong_ext':
+							echo 'Image Type Not Supported!';
+							break;
+						
+						default:
+							// code...
+							break;
+					}		}
+	}
+}
 ?>
 
 <!DOCTYPE html>
@@ -14,8 +37,55 @@ if($get_images === false){
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>PHP image gallery</title>
+	<style>
+		*{
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
+		.container{
+			display: grid;
+			grid-template-columns: 1fr 1fr 1fr;
+			max-width: 90%;
+			margin: auto;
+			min-height: 100vh;
+			min-height: 100lvh;
+			place-items: center;
+		}
+		header{
+			padding: 10px;
+			margin: 10px;
+			width: 95%;
+			background: #0000f0;
+			outline: 1px solid #fff;
+			border-radius: 10px;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+		}
+		.input input{
+				padding: 10px;
+				border:none;
+				cursor: pointer;
+		}
+		.input input[type=submit]{
+			background: #0f0;
+			border-radius: 10px;
+		}
+	</style>
 </head>
 <body>
+	<header>
+		<div class="title">
+			<h2>Kazi Mohammad Foysal</h2>
+		</div>
+		<div class="input">
+			<form enctype="multipart/form-data" method="post">
+				<input type="file" name="image">
+				<input type="submit" name="submit">
+			</form>
+		</div>
+	</header>
 	<div class="container">
 		<?php
 		foreach($get_images as $image){
